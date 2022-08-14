85fd5d849fe78c89a6a36f618316f6c2111109bd
xujinpeng <rabbirbot00@gmail.com>
2022-08-14 20:51:12 +0800

    feat: 完成 database 数据库连接驱动器的开发和 orm 基础类对数据库进行增删改查操作的功能

--- database.py
+++ database.py
@@ -9,14 +9,173 @@
 # @DESCRIPTION: 数据库连接工具
 
 
+from typing import Tuple
+import pymysql
 import psycopg2
+import psycopg2.extras
 
 
-class driver():
+class Driver():
+    """
+    数据库连接驱动器
+    """
 
-    def __init__(self, type, username, password, host, port):
+    def __init__(self, type_, host, port: int, username, password, database):
         """
         初始化
         """
-        pass
+        self.type_ = type_
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        # 数据库连接和光标
+        self.connection = None
+        self.cursor = None
 
+    def connect(self):
+        """
+        建立数据库连接
+        """
+        # PostgreSQL 数据库
+        if (self.type_.lower() == "postgresql"):
+            self.connection = psycopg2.connect(
+                database=self.database,
+                user=self.username,
+                password=self.password,
+                host=self.host,
+                port=self.port)
+            self.cursor = self.connection.cursor(
+                cursor_factory=psycopg2.extras.RealDictCursor)
+        # MySQL 数据库
+        elif (self.type_.lower() == "mysql"):
+            self.connection = pymysql.connect(
+                database=self.database,
+                user=self.username,
+                password=self.password,
+                host=self.host,
+                port=self.port)
+            self.cursor = self.connection.cursor(
+                cursor=pymysql.cursors.DictCursor)
+        else:
+            print(f"暂不支持该数据库类型：{self.type_}")
+
+    def execute(self,
+                action,
+                sql,
+                values: list = [],
+                filter_values: list = []) -> Tuple[int, list]:
+        """
+        执行 SQL
+        """
+        # PostgreSQL 数据库
+        if (self.type_.lower() == "postgresql"):
+            if (action == "select"):
+                self.cursor.execute(sql, filter_values)
+                row_count = self.cursor.rowcount
+                return row_count, self.cursor.fetchall()
+            elif (action == "insert"):
+                self.cursor.execute(sql, values)
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            elif (action == "update"):
+                self.cursor.execute(sql, [*values, *filter_values])
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            elif (action == "delete"):
+                self.cursor.execute(sql, filter_values)
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            else:
+                self.cursor.execute(sql, [*values, *filter_values])
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+        # MySQL 数据库
+        elif (self.type_.lower() == "mysql"):
+            self.connection.commit()
+            if (action == "select"):
+                self.cursor.execute(sql, filter_values)
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, self.cursor.fetchall()
+            elif (action == "insert"):
+                self.cursor.execute(sql, values)
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            elif (action == "update"):
+                self.cursor.execute(sql, [*values, *filter_values])
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            elif (action == "delete"):
+                self.cursor.execute(sql, filter_values)
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+            else:
+                self.cursor.execute(sql, [*values, *filter_values])
+                row_count = self.cursor.rowcount
+                self.connection.commit()
+                return row_count, []
+        else:
+            print(f"暂不支持该数据库类型：{self.type_}")
+
+    def test(self):
+        """
+        测试连接
+        """
+        test_sql = "SELECT 1;"
+        return self.execute("select", test_sql)
+
+    def select(self, sql, filter_values):
+        """
+        检索
+        """
+        return self.execute("select", sql, filter_values=filter_values)
+
+    def insert(self, sql, values):
+        """
+        插入
+        """
+        return self.execute("insert", sql, values=values)
+
+    def update(self, sql, values, filter_values):
+        """
+        更新
+        """
+        return self.execute(
+            "update", sql, values=values, filter_values=filter_values)
+
+    def delete(self, sql, filter_values):
+        """
+        删除
+        """
+        return self.execute("delete", sql, filter_values=filter_values)
+
+    def close(self):
+        """
+        关闭连接
+        """
+        self.cursor.close()
+        self.connection.close()
+
+
+# 临时测试
+if __name__ == "__main__":
+    test_driver = Driver(
+        type_="mysql",
+        host="xxxxx",
+        port=3306,
+        username="root",
+        password="xxxxx",
+        database="xxxxx"
+    )
+    test_driver.connect()
+    print(test_driver.test())
+    test_driver.close()
--- orm.py
+++ orm.py
@@ -10,6 +10,7 @@
 
 
 import copy
+import database
 
 
 # 检索 SQL
@@ -19,7 +20,7 @@ SELECT
 FROM
     {_table_name}
 WHERE
-    {filter}
+    {filters_placeholder}
 """
 
 # 插入 SQL
@@ -27,7 +28,7 @@ INSERT_SQL = """
 INSERT INTO
     {_table_name}({_column_names})
 VALUES(
-    {insert_values}
+    {insert_values_placeholder}
 ) ON CONFLICT DO NOTHING
 """
 
@@ -36,9 +37,9 @@ UPDATE_SQL = """
 UPDATE
     {_table_name}
 SET
-    {update_values}
+    {update_values_placeholder}
 WHERE
-    {filter}
+    {filters_placeholder}
 """
 
 # 插入冲突时更新 SQL
@@ -46,18 +47,18 @@ INSERT_ON_CONFLICT_DO_UPDATE = """
 INSERT INTO
     {_table_name}({_column_names})
 VALUES(
-    {insert_values}
+    {insert_values_placeholder}
 ) ON CONFLICT(
     {_primary_keys}
-) DO UPDATE SET {update_values}
+) DO UPDATE SET {update_values_placeholder}
 """
 
 # 删除 SQL
 DELETE_SQL = """
 DELETE FROM
-    {table_name}
+    {_table_name}
 WHERE
-    {filter}
+    {filters_placeholder}
 """
 
 
@@ -92,7 +93,7 @@ class BaseClass():
         字典转类
         """
         temp_dict = copy.deepcopy(dict_)
-        for key in temp_dict.keys():
+        for key in dict_.keys():
             if (key.startswith("_")):
                 temp_dict.pop(key)
         self.__dict__.update(temp_dict)
@@ -102,44 +103,200 @@ class BaseClass():
         类转字典
         """
         temp_dict = copy.deepcopy(self.__dict__)
-        for key in temp_dict.keys():
+        for key in self.__dict__.keys():
             if (key.startswith("_")):
                 temp_dict.pop(key)
         return temp_dict
 
-    def select(self):
+    def select(self, database_driver: database.Driver):
         """
         检索
-        """
-        pass
 
-    def insert(self):
+        默认根据本对象的主键进行搜索，某主键为空的时候会跳过该主键。
+        """
+        # 筛选用的参数 ["1 = 1", "column_01 = %s"]
+        filters = ["1 = 1"]
+        # 筛选实际用的值
+        filter_values = []
+        _dict = self._to_dict()
+        for key in _dict.keys():
+            if (key in self._primary_keys):
+                value = _dict[key]
+                # 主键有值才作为筛选条件
+                if (value is not None):
+                    filters.append(f"{key} = %s")
+                    filter_values.append(value)
+                else:
+                    pass
+            else:
+                pass
+        # 拼接检索 SQL
+        select_sql = SELECT_SQL.format(
+            _column_names=", ".join(self._column_names),
+            _table_name=self._table_name,
+            filters_placeholder="\nAND ".join(filters))
+        # print(select_sql, "\n", filter_values)
+        return database_driver.select(select_sql, filter_values)
+
+    def insert(self, database_driver: database.Driver):
         """
         插入
-        """
-        pass
 
-    def update(self):
+        返回成功插入的条数。
+        """
+        # 插入用的值
+        _dict = self._to_dict()
+        insert_values = [_dict[key] for key in self._column_names]
+        # 拼接插入 SQL
+        insert_sql = INSERT_SQL.format(
+            _table_name=self._table_name,
+            _column_names=", ".join(self._column_names),
+            insert_values_placeholder=", ".join(
+                ["%s"]*len(self._column_names)))
+        # print(insert_sql, "\n", insert_values)
+        return database_driver.insert(insert_sql, insert_values)
+
+    def update(self, database_driver: database.Driver):
         """
         更新
-        """
-        pass
 
-    def insert_on_conflict_do_update(self):
+        返回成功更新的条数；只保留主键更新整条数据。
+        """
+        # 筛选用的参数
+        filters = ["1 = 1"]
+        # 筛选实际用的值
+        filter_values = []
+        _dict = self._to_dict()
+        for key in _dict.keys():
+            if (key in self._primary_keys):
+                value = _dict[key]
+                # 主键有值才作为筛选条件
+                if (value is not None):
+                    filters.append(f"{key} = %s")
+                    filter_values.append(value)
+                else:
+                    pass
+            else:
+                pass
+        # 更新用的值
+        update_values = []
+        update_values_placeholder_elements = []
+        _dict = self._to_dict()
+        for key in self._column_names:
+            if (key not in self._primary_keys):
+                update_values.append(_dict[key])
+                update_values_placeholder_elements.append(f"{key} = %s")
+        # 拼接更新 SQL
+        update_sql = UPDATE_SQL.format(
+            _table_name=self._table_name,
+            update_values_placeholder=", ".join(
+                update_values_placeholder_elements),
+            filters_placeholder="\nAND ".join(filters))
+        # print(update_sql, "\n", update_values, "\n", filter_values)
+        return database_driver.update(update_sql, update_values, filter_values)
+
+    def insert_on_conflict_do_update(self, database_driver: database.Driver):
         """
         插入冲突时更新
-        """
 
-    def delete(self):
+        返回成功插入或更新的条数。
+        """
+        update_values_placeholder_elements = []
+        _dict = self._to_dict()
+        for key in self._column_names:
+            if (key not in self._primary_keys):
+                update_values_placeholder_elements.append(
+                    f"{key} = excluded.{key}")
+        insert_values = [_dict[key] for key in self._column_names]
+        insert_on_conflict_do_update_sql = INSERT_ON_CONFLICT_DO_UPDATE.format(
+            _table_name=self._table_name,
+            _column_names=", ".join(self._column_names),
+            insert_values_placeholder=", ".join(
+                ["%s"]*len(self._column_names)),
+            _primary_keys=", ".join(self._primary_keys),
+            update_values_placeholder=", ".join(
+                update_values_placeholder_elements))
+        # print(insert_on_conflict_do_update_sql, "\n", insert_values)
+        return database_driver.execute(
+            "insert_on_conflict_do_update",
+            insert_on_conflict_do_update_sql,
+            insert_values)
+
+    def delete(self, database_driver: database.Driver):
         """
         删除
+
+        返回删除的条数。
         """
-        pass
+        # 筛选用的参数
+        filters = ["1 = 1"]
+        # 筛选实际用的值
+        filter_values = []
+        _dict = self._to_dict()
+        for key in _dict.keys():
+            if (key in self._primary_keys):
+                value = _dict[key]
+                # 主键有值才作为筛选条件
+                if (value is not None):
+                    filters.append(f"{key} = %s")
+                    filter_values.append(value)
+                else:
+                    pass
+            else:
+                pass
+        # 拼接删除用 SQL
+        delete_sql = DELETE_SQL.format(
+            _table_name=self._table_name,
+            filters_placeholder="\nAND ".join(filters))
+        # print(delete_sql, "\n", filter_values)
+        return database_driver.delete(delete_sql, filter_values)
 
 
+# 临时测试
 if __name__ == "__main__":
-    """
-    测试
-    """
+    import uuid
+    import datetime
+    # 建立数据库连接
+    test_database_driver = database.Driver(
+        type_="postgresql",
+        host="xxxxx",
+        port=5432,
+        username="postgres",
+        password="xxxxx",
+        database="xxxxx"
+    )
+    test_database_driver.connect()
+    # 测试对象
     test_base_class = BaseClass()
-    print(test_base_class._column_names)
+    test_base_class._table_name = "test"
+    # 测试检索
+    # print(test_base_class.select(test_database_driver))
+    # 测试插入
+    # test_base_class.id = uuid.uuid1().hex
+    # test_base_class.create_user = "test_user"
+    # test_base_class.create_time = datetime.datetime.now(
+    #     datetime.timezone.utc)
+    # print(test_base_class.insert(test_database_driver))
+    # 测试更新
+    # count, select_result = test_base_class.select(test_database_driver)
+    # test_base_class._to_class(select_result[0])
+    # test_base_class.update_user = "test_user"
+    # test_base_class.update_time = datetime.datetime.now(
+    #     datetime.timezone.utc)
+    # print(test_base_class.update(test_database_driver))
+    # 测试删除
+    # count, select_result = test_base_class.select(test_database_driver)
+    # test_base_class._to_class(select_result[0])
+    # print(test_base_class.delete(test_database_driver))
+    # 测试冲突时更新
+    test_base_class.id = uuid.uuid1().hex
+    test_base_class.create_user = "test_user"
+    test_base_class.create_time = datetime.datetime.now(
+        datetime.timezone.utc)
+    print(test_base_class.insert_on_conflict_do_update(test_database_driver))
+    count, select_result = test_base_class.select(test_database_driver)
+    test_base_class._to_class(select_result[0])
+    test_base_class.create_user = "changed_test_user"
+    print(test_base_class.insert_on_conflict_do_update(test_database_driver))
+    # 关闭数据库连接
+    test_database_driver.close()
